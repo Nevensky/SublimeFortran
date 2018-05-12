9f3c3045c812d9bd6cb183e6bda8b88cdf9877d0
jwortmann <janos.wortmann@web.de>
2018-05-12 19:58:57 +0200

    Add compatibility for new color scheme format

--- InlineLatexHover.py
+++ InlineLatexHover.py
@@ -58,62 +58,75 @@ class InlineLatexHover(sublime_plugin.EventListener):
         # It also has one dict with just "settings", which has non-scope related settings (typically first in list)
         # To get color scheme for a particular scope, find the dict with the scope that matches best.
         scheme_path = view.settings().get("color_scheme")
-        scheme_content = sublime.load_binary_resource(scheme_path)
-        scheme_data = plistlib.readPlistFromBytes(scheme_content)
+        
+        # Check color scheme file format
+        if scheme_path.lower().endswith(".sublime-color-scheme"):
+            color_dict = view.style()
+            bg = color_dict["background"]
+            fg = color_dict["foreground"]
+            # Remove leading #
+            bg = bg[1:]
+            fg = fg[1:]
+            
+        elif scheme_path.lower().endswith(".tmtheme"):
+            scheme_content = sublime.load_binary_resource(scheme_path)
+            scheme_data = plistlib.readPlistFromBytes(scheme_content)
 
-        def parse_popupCss(css):
-            words = css.split()
-            i = 0
-            bg = None
-            fg = None
-            while words[i] != "html":
-                i += 1
-            while words[i] != "}":
-                if words[i] == "background-color:":
-                    bg = words[i+1]
-                if words[i] == "color:":
-                    fg = words[i+1]
-                i += 1
+            def parse_popupCss(css):
+                words = css.split()
+                i = 0
+                bg = None
+                fg = None
+                while words[i] != "html":
+                    i += 1
+                while words[i] != "}":
+                    if words[i] == "background-color:":
+                        bg = words[i+1]
+                    if words[i] == "color:":
+                        fg = words[i+1]
+                    i += 1
 
-            # Defaults if not found
-            if bg == None:
-                bg = "#FFFFFF"
-            if fg == None:
-                fg = "#000000"
+                # Defaults if not found
+                if bg == None:
+                    bg = "#FFFFFF"
+                if fg == None:
+                    fg = "#000000"
 
-            # Remove leading # and trailing ;
-            bg = bg[1:7]
-            fg = fg[1:7]
-            return bg, fg
+                # Remove leading # and trailing ;
+                bg = bg[1:7]
+                fg = fg[1:7]
+                return bg, fg
 
-        try:
-            # Get colors from popupCss
-            css = scheme_data["settings"][0]["settings"]["popupCss"]
-            bg, fg = parse_popupCss(css)
-        except KeyError:
             try:
-                # Get colors from the main section of scheme_data["settings"]
-                bg = scheme_data["settings"][0]["settings"]["background"][1:]
-                fg = scheme_data["settings"][0]["settings"]["foreground"][1:]
+                # Get colors from popupCss
+                css = scheme_data["settings"][0]["settings"]["popupCss"]
+                bg, fg = parse_popupCss(css)
             except KeyError:
-                bg = "000000"
-                fg = "FFFFFF"
+                try:
+                    # Get colors from the main section of scheme_data["settings"]
+                    bg = scheme_data["settings"][0]["settings"]["background"][1:]
+                    fg = scheme_data["settings"][0]["settings"]["foreground"][1:]
+                except KeyError:
+                    bg = "000000"
+                    fg = "FFFFFF"
 
-        # # theme_datas contains a list of lists of dicts with theme properties.
-        # # Each item in the top-level list represents a resource,
-        # # i.e. the original theme file, theme addons, user modifications, etc in resource order
-        # # I guess we want the last one? theme_data = theme_datas[-1]
-        # # theme_data is a list of dicts with keys:
-        # #   "class": "tab_control", "icon_button_control", etc
-        # #   "attributes": "right", "dirty", "selected" etc
-        # #   "layer3.opacity": 0.75 etc
-        # #   "settings": [list of settings that are set to true for this to be applied]
-        # theme_filename = sublime.load_settings("Preferences.sublime-settings").get("theme")
-        # theme_paths = sublime.find_resources(theme_filename)
-        # theme_contents = [sublime.load_resource(x) for x in theme_paths]
-        # theme_datas = [sublime.decode_value(x) for x in theme_contents]
-        # theme_data = theme_datas[-1]
+            # # theme_datas contains a list of lists of dicts with theme properties.
+            # # Each item in the top-level list represents a resource,
+            # # i.e. the original theme file, theme addons, user modifications, etc in resource order
+            # # I guess we want the last one? theme_data = theme_datas[-1]
+            # # theme_data is a list of dicts with keys:
+            # #   "class": "tab_control", "icon_button_control", etc
+            # #   "attributes": "right", "dirty", "selected" etc
+            # #   "layer3.opacity": 0.75 etc
+            # #   "settings": [list of settings that are set to true for this to be applied]
+            # theme_filename = sublime.load_settings("Preferences.sublime-settings").get("theme")
+            # theme_paths = sublime.find_resources(theme_filename)
+            # theme_contents = [sublime.load_resource(x) for x in theme_paths]
+            # theme_datas = [sublime.decode_value(x) for x in theme_contents]
+            # theme_data = theme_datas[-1]
+        # Default values in case of unknown format 
+        else:
+            bg = "000000"
+            fg = "FFFFFF"
 
         return (bg, fg)
-
-
